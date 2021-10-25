2ce65bb654a708d6079cd7f5f4fab77140b4cb83
Acelele007 <nalivkin.d@gmail.com>
2021-10-25 05:51:35 +0300

    Start of constructors

---
+++ Square3x3.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Square3x3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Square3x3
+{
+    // instance variables - replace the example below with your own
+    private int [][] matrix3x3; 
+
+    /**
+     * Constructor for objects of class Square3x3
+     */
+    public Square3x3()
+    {
+        matrix3x3 = {{-1},{-1},{-1}}
+    }
+    /**
+     * Constructor for objects of class Square3x3
+     */
+    public Square3x3(int [][] array)
+    {
+        
+    }
+    /**
+     * Constructor for objects of class Square3x3
+     */
+    public Square3x3(Square3x3 other)
+    {
+        
+    }
+
+}
