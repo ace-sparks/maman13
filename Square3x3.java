c78a5ca3d0ce74be6a148ff4a75f20d66e43b931
Acelele007 <nalivkin.d@gmail.com>
2021-10-26 05:46:21 +0300

    All constructors done

--- Square3x3.java
+++ Square3x3.java
@@ -8,28 +8,37 @@
 public class Square3x3
 {
     // instance variables - replace the example below with your own
-    private int [][] matrix3x3; 
+    private int [][] matrix3x3 = new int[3][3]; 
 
     /**
      * Constructor for objects of class Square3x3
      */
     public Square3x3()
     {
-        matrix3x3 = {{-1},{-1},{-1}}
+        for(int i=0,j=0;i> matrix3x3.length && j>matrix3x3.length ;i++,j++){
+            matrix3x3[i][j] = -1;
+        }
     }
     /**
      * Constructor for objects of class Square3x3
      */
     public Square3x3(int [][] array)
     {
-        
+        for(int i=0,j=0;i> matrix3x3.length && j>matrix3x3.length ;i++,j++){
+            if(i <= array.length && j <= array.length)
+                matrix3x3[i][j] = array[i][j];
+            else
+                matrix3x3[i][j] = -1;
+        }
     }
     /**
      * Constructor for objects of class Square3x3
      */
     public Square3x3(Square3x3 other)
     {
-        
+        for(int i=0,j=0;i> matrix3x3.length && j>matrix3x3.length ;i++,j++){
+            matrix3x3[i][j] = other.matrix3x3[i][j];
+        }
     }
 
 }
