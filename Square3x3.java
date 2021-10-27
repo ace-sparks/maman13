3bd19477ad677e222d0ff1924ad6e81dc3724034
Acelele007 <nalivkin.d@gmail.com>
2021-10-27 04:57:11 +0300

    First class done WITHOUT API

--- Square3x3.java
+++ Square3x3.java
@@ -7,37 +7,125 @@
  */
 public class Square3x3
 {
-    // instance variables - replace the example below with your own
-    private int [][] matrix3x3 = new int[3][3]; 
-
-    /**
+    // instance variables
+    private int [][] matrix = new int[3][3]; 
+    /** Constructor 1
      * Constructor for objects of class Square3x3
      */
     public Square3x3()
     {
-        for(int i=0,j=0;i> matrix3x3.length && j>matrix3x3.length ;i++,j++){
-            matrix3x3[i][j] = -1;
-        }
+        matrix = new int[][]{{-1,-1,-1},{-1,-1,-1},{-1,-1,-1}};
     }
-    /**
+
+    /** Constructor 2
      * Constructor for objects of class Square3x3
      */
-    public Square3x3(int [][] array)
-    {
-        for(int i=0,j=0;i> matrix3x3.length && j>matrix3x3.length ;i++,j++){
-            if(i <= array.length && j <= array.length)
-                matrix3x3[i][j] = array[i][j];
-            else
-                matrix3x3[i][j] = -1;
+    public Square3x3(int [][] array){
+        for(int i = 0; i < 3 ; i++){
+            for(int j = 0; j < 3 ; j++){
+                if(j < array[0].length && i < array.length )
+                    matrix[i][j] = array[i][j];
+                else
+                    matrix[i][j] = -1;
+            }
         }
     }
-    /**
+
+    /** Copy constructor
      * Constructor for objects of class Square3x3
      */
     public Square3x3(Square3x3 other)
     {
-        for(int i=0,j=0;i> matrix3x3.length && j>matrix3x3.length ;i++,j++){
-            matrix3x3[i][j] = other.matrix3x3[i][j];
+        for(int i = 0; i < 3 ; i++){
+            for(int j = 0; j < 3 ; j++){
+                matrix[i][j] = other.matrix[i][j];
+            }
+        }
+    }
+
+    /** getCell
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getCell(int row, int col)
+    {
+        return matrix[row][col];
+    }
+
+    /** setXY
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void setXY(int row, int col, int value)
+    {
+        matrix[row][col] = value;
+    }
+
+    /** toString
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String toString()
+    {
+        String printString = "";
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                printString = printString + matrix[i][j] + "\t";
+                if(j == 2)
+                    printString = printString + "\n";
+            }
+        }
+        return printString;
+    }
+
+    /** allThere
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public boolean allThere()
+    {
+        for(int i =0 ; i < matrix.length; i++){
+            for(int j =0 ; j < matrix[0].length; j++){
+                if(matrix[i][j] < 1 || matrix[i][j] > 9){
+                    return false;
+                }
+            } 
+        }
+        return true;
+    }
+
+    /** whosThereRow
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void whosThereRow(int row, boolean[] values)
+    {
+        for(int i = 0 ; i < matrix.length; i++){
+            if(matrix[row][i] > 0 && matrix[row][i] < 10)
+                values[matrix[row][i]] = true;
+        }
+    }
+    /** whosThereCol
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void whosThereCol(int col, boolean[] values)
+    {
+        for(int i = 0 ; i < matrix.length; i++){
+            if(matrix[i][col] > 0 && matrix[i][col] < 10)
+                values[matrix[i][col]] = true;
         }
     }
 
